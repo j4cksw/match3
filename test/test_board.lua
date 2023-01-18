9432c766023f3b8db56642f384a42d03ff0d7e16
PRAYOCH R <prayoch@clockupstudio.com>
2023-01-18 18:06:30 +0700

    refactored generating test examples

--- test/test_board.lua
+++ test/test_board.lua
@@ -1,27 +1,21 @@
 function test_checkMatchesAvailable()
-    local tiles = {
-        { Tile(1, 1, 1, 1, 0),
-            Tile(1, 2, 2, 1, 0),
-            Tile(1, 3, 1, 1, 0),
-            Tile(1, 4, 1, 1, 0),
-            Tile(1, 5, 3, 1, 0),
-            Tile(1, 6, 4, 1, 0),
-            Tile(1, 7, 5, 1, 0),
-            Tile(1, 8, 6, 1, 0) },
-    }
-    assert_equal(1, countAvailableMatches(tiles))
-    local tiles = {
-        { 
-            Tile(1, 1, 2, 1, 0),
-            Tile(1, 2, 3, 1, 0),
-            Tile(1, 3, 2, 1, 0),
-            Tile(1, 4, 2, 1, 0),
-            Tile(1, 5, 1, 1, 0),
-            Tile(1, 6, 4, 1, 0),
-            Tile(1, 7, 1, 1, 0),
-            Tile(1, 8, 1, 1, 0) },
-    }
-    assert_equal(2, countAvailableMatches(tiles))
+
+    local tiles_1 = generateTilesFromColors({{ 1, 2, 1, 1, 3, 4, 5, 6}})
+    assert_equal(1, countAvailableMatches(tiles_1))
+
+    local tiles_2 = generateTilesFromColors({{ 2, 3, 2, 2, 1, 4, 1, 1}})
+    assert_equal(2, countAvailableMatches(tiles_2))
+end
+
+function generateTilesFromColors(colors)
+    local tiles = {}
+    for tileY = 1, #colors do
+        table.insert(tiles, {})
+        for tileX = 1, #colors[tileY] do
+            table.insert(tiles[tileY], Tile(tileX, tileY, colors[tileY][tileX], 0, 0))
+        end
+    end
+    return tiles
 end
 
 function generateTestTiles()
