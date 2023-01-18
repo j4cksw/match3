f384ceecfd34632bcf24470dd1f945cb002ceb2c
PRAYOCH R <prayoch@clockupstudio.com>
2023-01-18 17:57:48 +0700

    Added test case for check if pattern available 2 times

--- test/test_board.lua
+++ test/test_board.lua
@@ -13,15 +13,15 @@ function test_checkMatchesAvailable()
     local tiles = {
         { 
             Tile(1, 1, 2, 1, 0),
-            Tile(1, 2, 1, 1, 0),
+            Tile(1, 2, 3, 1, 0),
             Tile(1, 3, 2, 1, 0),
-            Tile(1, 4, 1, 1, 0),
+            Tile(1, 4, 2, 1, 0),
             Tile(1, 5, 1, 1, 0),
             Tile(1, 6, 4, 1, 0),
-            Tile(1, 7, 5, 1, 0),
-            Tile(1, 8, 6, 1, 0) },
+            Tile(1, 7, 1, 1, 0),
+            Tile(1, 8, 1, 1, 0) },
     }
-    assert_equal(1, countAvailableMatches(tiles))
+    assert_equal(2, countAvailableMatches(tiles))
 end
 
 function generateTestTiles()
